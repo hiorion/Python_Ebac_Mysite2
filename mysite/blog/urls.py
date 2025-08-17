754979b5fba909d1477bdc62d329a50dbaa5b749
henry orion <hen.orion@gmail.com>
2025-08-17 00:53:25 -0300

    templates set

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views.post_view import PostView
+from blog.views import PostView, PostDetail
 
 urlpatterns = [
     path("", PostView.as_view(), name="home"),
+    path("<slug:slug>/", PostDetail.as_view(), name="post_detail")
 ]
